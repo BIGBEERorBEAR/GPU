6330d3880632a9041b33ace2cb52a325bb4523dd
Francois Bolot <francois.bolot@epita.fr>
2023-03-23 12:08:42 +0100

    exo2 first commit

--- TP2/tp2/ex2_stride.cu
+++ TP2/tp2/ex2_stride.cu
@@ -7,24 +7,62 @@ inline void cuda_check(cudaError_t code, const char *file, int line) {
     }
 }
 
+__global__ void add(float *x, float *y, int N) {
+    int thr_id = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    int i = thr_id;
+
+    while (i < N)
+    {
+        y[i] += x[i];
+        i += stride;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     const int N = argc >= 2 ? std::stoi(argv[1]) : 1e6;
     std::cout << "N = " << N << std::endl;
 
     float *x, *y;
+    float *array_x, *array_y;
     x = (float*)malloc(N * sizeof(float));
     y = (float*)malloc(N * sizeof(float));
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) 
+    {
         x[i] = 1.0f;
         y[i] = 2.0f;
     }
 
-    // ...
+    cudaMalloc(&array_x, N * sizeof(float));
+    cudaMalloc(&array_y, N * sizeof(float));
+    cudaMemcpy(array_x, x, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(array_y, y, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    add<<<(N+255)/256, 256>>>(array_x, array_y, N);
+    CUDA_CHECK(cudaDeviceSynchronize());
+    int i = 0;
+
+    while (i < N)
+    {
+        if (array_y[i] == 3.0f)
+        {
+            std::cout << "y[" << i << "] = " << array_y[i] << std::endl;
+            i++;
+            continue;
+        }
+        else
+        {
+            std::cerr << "Error at position " << i << std::endl;
+            return 1;
+        }
+    }
 
     free(x);
     free(y);
+    cudaFree(array_x);
+    cudaFree(array_y);
 
     return 0;
 }
